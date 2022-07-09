2b94ae8a295fe19132a8518908bc42432b5b164b
Vrfcbv <105105382+Vrfcbv@users.noreply.github.com>
2022-07-09 23:33:34 +0300

    Лаба 7
    prak73 - работа с файлами
    Данная Лаба содержит отчёт в репозитории

---
+++ prak71.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n; cout << "Введите размерность матрицы n*n: "; cin >> n; vector <vector<int> > m(n);
+	vector <int> a, b, c;
+	for (int i = 0; i < n; i++) {m[i].resize(n);
+	for (int j = 0; j < n; j++) {cin >> m[i][j];
+	if (m[i][j] < 0) a.push_back(m[i][j]);
+	if (m[i][j] % 2 == 0) b.push_back(m[i][j]); 
+	else c.push_back(m[i][j]);} }
+	cout << "a) Отрицательные: "; for (int i = 0; i < a.size(); i++) cout << a[i] << " ";
+	cout << "\nб) Чётные: "; for (int i = 0; i < b.size(); i++) cout << b[i] << " ";
+	cout << "\nб) Нечётные: "; for (int i = 0; i < c.size(); i++) cout << c[i] << " ";
+	return 0;}
---
+++ prak73.cpp
@@ -0,0 +1,14 @@
+﻿#include <fstream>
+#include <vector>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	ifstream cin("input.txt");
+	ofstream cout("output.txt");
+	vector <pair<string, int> > buk;
+	for (int j = 0; j < 15; j++) { string type; int stoi; cin >> type >> stoi; buk.push_back(make_pair(type, stoi)); }
+	int sumsrstoi = 0, kolla = 0;
+	for (int j = 0; j < buk.size(); j++)
+		if (buk[j].first == "Легковой" || buk[j].first == "легковой") { sumsrstoi += buk[j].second; kolla++; }
+	if (kolla != 0) cout << "Средняя стоимость легковых автомобилей:\n" << (double)sumsrstoi / kolla;
+	else cout << "Нет легковых автомобилей!"; return 0;}
