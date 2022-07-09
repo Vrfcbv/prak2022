934d854699870edd0e80c3200eb8b9c7a8a87853
Vrfcbv <105105382+Vrfcbv@users.noreply.github.com>
2022-07-09 23:34:43 +0300

    Лаба 8
    Работа с файлами

---
+++ prak81.cpp
@@ -0,0 +1,12 @@
+﻿#include <fstream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	ifstream cin("input.txt");
+	ofstream cout("output.txt");
+	int f = 0, fl = 0;
+	while (!cin.eof()) {int a; cin >> a;
+		if (f == 0) { cout << a << " "; } else f = 0;
+		if (a == 0 && fl == 0) { f = 1; fl = 1; } }
+	cin.close(); cout.close();
+	return 0;}
---
+++ prak83.cpp
@@ -0,0 +1,12 @@
+﻿#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	ifstream cin("input.txt");
+	ofstream cout("output.txt");
+	while (!cin.eof()) {
+		string a; getline(cin, a, '\n');
+		cout << a << "!\n"; }
+	cin.close(); cout.close();
+	return 0;}
